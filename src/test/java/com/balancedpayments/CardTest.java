4bc6b50623a9d9ab9bab0e9848cafafd0f463434
Parham Negahdar <pnegahdar@gmail.com>
2013-06-27 22:05:18 -0700

    Documentation and shit

---
+++ src/test/java/com/balancedpayments/CardTest.java
@@ -0,0 +1,27 @@
+package com.balancedpayments;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.balancedpayments.errors.CannotCreate;
+import com.balancedpayments.errors.HTTPError;
+import com.balancedpayments.errors.NotCreated;
+
+
+public class CardTest  extends BaseTest {
+
+    protected Card card;
+
+    @Test
+    public void testVerify() throws CannotCreate, HTTPError, NotCreated {
+        Customer buyer = createBusinessCustomer();
+        Card card = createCard(mp);
+        buyer.addCard(card);
+        buyer.addCard(createCard(mp));
+        card.delete();
+        //Some assertion here...
+    }
+}
