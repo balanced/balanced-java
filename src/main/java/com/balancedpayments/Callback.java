16b08f9198bf632e32d17fa1d9a6c1d6b9b06147
Ben Mills <ben@unfiniti.com>
2013-10-06 14:59:41 -0600

    Add callback method

--- src/main/java/com/balancedpayments/Callback.java
+++ src/main/java/com/balancedpayments/Callback.java
@@ -12,17 +12,27 @@ public class Callback extends Resource {
     @ResourceField(mutable=true)
     public String url;
 
+    @ResourceField(mutable=true, required=false)
+    public String method;
+
     public static class Collection extends ResourceCollection<Callback> {
         public Collection(String uri) {
             super(Callback.class, uri);
         }
 
-        public Callback create(String url) throws HTTPError {
+        public Callback create(String url, String method) throws HTTPError {
             Callback callback = new Callback();
             callback.uri = getURI();
             callback.url = url;
+            if (method != null) {
+                callback.method = method;
+            }
             callback.save();
             return callback;
+        }
+
+        public Callback create(String url) {
+            return create(url);
         }
     };
 
