52e44ea9bb184442ac791d9a0361b1ee11dafb61
Ben Mills <ben@unfiniti.com>
2014-03-10 09:45:41 -0600

    Updated Quickstart

--- src/examples/com/balancedpayments/QuickStart.java
+++ src/examples/com/balancedpayments/QuickStart.java
@@ -3,10 +3,7 @@ package com.balancedpayments;
 import java.util.HashMap;
 import java.util.Map;
 
-import com.balancedpayments.errors.HTTPError;
-import com.balancedpayments.errors.MultipleResultsFound;
-import com.balancedpayments.errors.NoResultsFound;
-import com.balancedpayments.errors.NotCreated;
+import com.balancedpayments.errors.*;
 
 public class QuickStart {
 
@@ -22,83 +19,56 @@ public class QuickStart {
         Marketplace mp = new Marketplace();
         mp.save();
 
-        System.out.printf("Who am I? Easy Merchant.me().uri -> %s\n", Merchant.me().uri);
-
-        System.out.printf("Whats my marketplace? Easy Marketplace.mine().uri -> %s\n", Marketplace.mine().uri);
+        System.out.printf("Whats my marketplace? Easy Marketplace.mine().href -> %s\n", Marketplace.mine().href);
 
         System.out.printf("My marketplace's name is: %s\n", mp.name);
-        System.out.printf("Changing it to TestFooey..\n");
+        System.out.printf("Changing it to TestFooey.\n");
         mp.name = "TestFooey";
         mp.save();
         System.out.printf("My marketplace's name is now: %s\n", mp.name);
 
-        System.out.printf("Cool, let's create (aka tokenize) a card!\n");
-        Card card = mp.cards.create("5105105105105100", 12, 2015);
-        System.out.printf("Our card is %s.\n", card.uri);
+        System.out.printf("Cool, let's create (tokenize) a card!\n");
 
-        System.out.printf("Now create our **buyer** account associated with that card\n");
-        Account buyer = mp.createBuyerAccount("buyer", "buyer@example.org", card.uri, null);
-        System.out.printf("Our **buyer** account is %s.\n", buyer.uri);
+        Map<String, Object> cardPayload = new HashMap<String, Object>();
+        cardPayload.put("name", "John Jameson");
+        cardPayload.put("number", "5105105105105100");
+        cardPayload.put("expiration_month", 12);
+        cardPayload.put("expiration_year", 2020);
+        cardPayload.put("cvv", "123");
 
-        System.out.printf("Lets cardHold some moneyfrom the buyer, say $15\n");
-        CardHold cardHold = buyer.hold(1500);
+        Card buyerCard = new Card(cardPayload);
+        buyerCard.save();
 
-        System.out.printf("And now capture that money\n");
+        System.out.printf("Our card is %s.\n", buyerCard.href);
+
+        System.out.println("Create a buyer customer.");
+
+        Map<String, Object> buyerPayload = new HashMap<String, Object>();
+        buyerPayload.put("name", "John Jameson");
+
+        Customer buyer = new Customer(buyerPayload);
+        buyer.save();
+        System.out.printf("Our buyer customer is %s.\n", buyer.href);
+
+        System.out.printf("Associate the card to the buyer.\n");
+        buyerCard.associateToCustomer(buyer);
+
+        System.out.printf("Create a $15 hold on the buyer's card.\n");
+        Map<String, Object> holdPayload = new HashMap<String, Object>();
+        holdPayload.put("amount", 1500);
+        CardHold cardHold = buyerCard.hold(holdPayload);
+
+        System.out.println("Now capture that money");
         Debit debit = cardHold.capture();
-        System.out.printf("Which gives us this debit %s\n", debit.uri);
+        System.out.printf("Which gives us this debit, %s\n", debit.href);
 
         mp.reload();
-        System.out.printf("Sweet, now we have this much in escrow %s\n", mp.in_escrow);
+        System.out.printf("Now we have %s in escrow.\n", mp.in_escrow);
 
-        System.out.printf("But lets refund the full amount\n");
+        System.out.printf("Now refund the full amount.\n");
         Refund refund = debit.refund();
 
         mp.reload();
-        System.out.printf("So now we have this much in escrow %s\n", mp.in_escrow);
-
-        System.out.printf("That was the buyer, now lets add a **merchant** account\n");
-
-        Map<String, Object> identity = new HashMap<String, Object>();
-        identity.put("type", "person");
-        identity.put("name", "Billy Jones");
-        identity.put("street_address", "801 High St");
-        identity.put("postal_code", "94301");
-        identity.put("country_code", "USA");
-        identity.put("dob", "1979-02");
-        identity.put("phone_number", "+16505551234");
-
-        BankAccount bank_account = mp.tokenizeBankAccount(
-                "Jack Q Merchant",
-                "123123123",
-                "123123123",
-                BankAccount.CHECKING);
-
-        Account merchant = mp.createMerchantAccount(
-                "Jack Q Merchant",
-                "merchant@example.org",
-                bank_account.uri,
-                identity,
-                null);
-        System.out.printf("Our **merchant** account is %s.\n", merchant.uri);
-
-        System.out.printf("Our **buyer** wants to purchase something from our **merchant** for $130.\n");
-        debit = buyer.debit(13000, "TestFooey.COM");
-
-        System.out.printf("Lets credit the **merchant** $110 of that $130.\n");
-        Credit credit = merchant.credit(11000, "Purchase on TestFooey");
-
-        System.out.printf("We are the marketplace and charge 15%%, so we just earned $20!\n");
-        mp.owner_account.credit(2000, "Commission for TestFooey");
-
-        System.out.printf("Now the **buyer** wants to deactivate their card.\n");
-        card.is_valid = false;
-        card.save();
-
-        System.out.printf("Hey I stored the URI, how can I get the object?\n");
-        Account.get(buyer.uri);
-        Credit.get(credit.uri);
-        Debit.get(debit.uri);
-
-        System.out.printf("And there you have it :)\n");
+        System.out.printf("Now we have %s in escrow.\n", mp.in_escrow);
     }
 }
