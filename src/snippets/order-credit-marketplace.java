987d60428421ee225108451846a947e470c344e0
richie serna <richie@balancedpayments.com>
2014-09-26 14:02:15 -0700

    Edit order-credit-marketplace

--- src/snippets/order-credit-marketplace.java
+++ src/snippets/order-credit-marketplace.java
@@ -1,5 +1,7 @@
-BankAccount bankAccount =  Marketplace.mine().owner_customer.bank_accounts.query().first();
+BankAccount marketplaceAccount =  Marketplace.mine().owner_customer.bank_accounts.query().first();
+
 HashMap<String, Object> payload = new HashMap<String, Object>();
 payload.put("amount", 2000);
 payload.put("description", "Credit from order escrow to marketplace bank account");
-Credit credit = bankAccount.credit(payload);
+
+Credit credit = order.creditTo(marketplaceAccount, payload);
