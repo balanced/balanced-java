c7c275bfa72f3fbc5f2861d2b6fe9f01766475c1
richie serna <richie@balancedpayments.com>
2014-11-12 13:48:20 -0800

    Add settlement and resource class

---
+++ src/main/java/com/balancedpayments/Account.java
@@ -0,0 +1,8 @@
+package com.balancedpayments;
+
+import com.balancedpayments.core.Resource;
+
+public class Account extends Resource {
+
+    public static final String resource_href = "/accounts";
+}
